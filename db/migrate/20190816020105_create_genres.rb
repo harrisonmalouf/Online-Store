709e4fd343c9cfa4e0b0c1588703fb778e548c26
AJ <aj.tauber98@gmail.com>
2019-08-17 16:00:48 +1000

    added genres to games and changed type to cat

--- db/migrate/20190816020105_create_genres.rb
+++ db/migrate/20190816020105_create_genres.rb
@@ -2,7 +2,7 @@ class CreateGenres < ActiveRecord::Migration[5.2]
   def change
     create_table :genres do |t|
       t.text :name
-      t.text :type
+      t.text :catagory
       t.text :image
 
       t.timestamps
