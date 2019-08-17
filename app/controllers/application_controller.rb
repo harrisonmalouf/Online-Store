d6c64aa6397d75a4f850e5f73074b592a0326c0f
Harrison Malouf <harrisonmalouf@gmail.com>
2019-08-17 20:35:11 +1000

    fixed up bug in app/controllers/application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,16 +3,16 @@ class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
 
   private
-  def fetch_user <#% this allows us to fetch a username if in session %>
+  def fetch_user
     @current_user = User.find_by :id => session[:user_id] if session[:user_id].present?
     session[:user_id] = nil unless @current_user.present?
   end
 
-  def check_for_login <#% This does exactly as it says %>
+  def check_for_login
     redirect_to login_path unless @current_user.present?
   end
 
-  def check_for_admin <#% This does exactly as it says %>
+  def check_for_admin 
     redirect_to root_path unless @current_user.present? && @current_user.admin?
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_08_17_054515) do
     t.float "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "Description"
     t.string "description"
     t.text "genre"
   end
